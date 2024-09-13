de8c88032f0637871fc0e81113f2fdf8ab94c0e9
YoyoLiuuu <liuyimeng01@gmail.com>
2024-09-13 11:29:10 -0400

    Update Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int initial = 100;
+        int steps = 0;
+        boolean done = false;
+        while (!done) {
+            boolean even = initial % 2 == 0;
+            if (even) {
+                initial = initial / 2;
+            }
+            else {
+                initial = initial - 1;
+            }
+            steps++;
+            done = initial == 0;
+        }
+        System.out.println(steps);
+    }
 }
